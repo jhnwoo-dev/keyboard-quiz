abd3e255a67e6242d31ddc57d0869e8a9c0f27d9
Jason Nguyen <jhnwoo.dev@gmail.com>
2023-01-04 22:38:54 -0800

    completed HTML coding, CSS coding partially completed, JavaScript coding began

---
+++ script.js
@@ -0,0 +1,21 @@
+//query selectors
+let startButton = document.getElementById('start-btn')
+let quizSection = document.getElementById('quiz-box')
+let quizTxt = document.getElementById('question-text')
+let timer = document.getElementById('timer')
+let choiceSection = document.getElementById('choice-box')
+let score = document.getElementById('score')
+
+//create an array of objects that represents the questions and their answers
+
+
+//need event listener for start button
+startButton.addEventListener('click', function () {
+    console.log("game start");
+
+    //display quiz section by removing is-hidden class
+    //display choice section by removing is-hidden class
+
+})
+
+//create a function that starts timer countdown and call within startButton function
